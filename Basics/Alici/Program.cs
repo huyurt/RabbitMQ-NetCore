c84c023d112d10881ce6a2a9527003a2878fc443
Hudayfe <hudayfeyurt@hotmail.com>
2019-08-07 00:27:02 +0300

    Alıcı ve gönderici için temel örnek

---
+++ Basics/Alici/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Alici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = Properties.Resources.HostName
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            string kuyruk = "TestKuyruğu";
+            channel.QueueDeclare(kuyruk, false, false, false, null);
+
+            var alici = new EventingBasicConsumer(channel);
+            alici.Received += (model, tasinan) =>
+            {
+                var mesajByte = tasinan.Body;
+                var mesaj = Encoding.UTF8.GetString(mesajByte);
+
+                Console.WriteLine("Mesaj alındı: " + mesaj);
+            };
+
+            channel.BasicConsume(kuyruk, true, alici);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basics/Gonderici/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Gonderici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = Properties.Resources.HostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            string kuyruk = "TestKuyruğu";
+            channel.QueueDeclare(kuyruk, false, false, false, null);
+
+            string mesaj = "Kuyruğa gönderilen, kuyruktan gelen mesaj";
+            var mesajByte = Encoding.UTF8.GetBytes(mesaj);
+
+            channel.BasicPublish("", kuyruk, null, mesajByte);
+
+            Console.WriteLine("Mesaj gönderildi: " + mesaj);
+            Console.ReadLine();
+        }
+    }
+}
